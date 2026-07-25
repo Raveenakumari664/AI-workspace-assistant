5e6e163ab7b8c04f79d2da71793bafdb48d2c21b
raveena <raveenakumari664@gmail.com>
2026-07-25 22:01:07 +0500

    phase 2: organize health route into api router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from app.api import health
 
 app = FastAPI()
+
+app.include_router(health.router)
 
 @app.get("/")
 def read_root():
     return {"message": "AI Workspace Assistant backend is running!"}
 
-@app.get("/health")
-def read_root():
-    return {"status":"OK"}
+# @app.get("/health")
+# def read_root():
+#     return {"status":"OK"}
